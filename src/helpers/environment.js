caccd0fa053ab8cda347cad81bd761ef5659437e
AlanJschultz <alanjschultz@gmail.com>
2020-02-16 16:17:39 -0500

    02162020 heroku changes

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -3,15 +3,18 @@
 let APIURL = '';
 
 switch (window.location.hostname) {
+
     // this is the local host name of your react app
     case 'localhost' || '127.0.0.1':
         // this is the local host name of your API
         APIURL = 'http://localhost:4000';
         break;
+        
         // this is the deployed react application
     case 'https://ajs-bowlinglog-client.herokuapp.com/':
         // this is the full url of your deployed API
         APIURL = 'https://ajs-bowlinglog-server.herokuapp.com';
+        break;
 }
 
 export default APIURL;
