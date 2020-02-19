0bb769558166107fd66482a42ca84c3639139f61
AlanJschultz <alanjschultz@gmail.com>
2020-02-18 21:50:04 -0500

    message

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,21 +1,10 @@
 // jshint esversion:6
 
 let APIURL = '';
-
-switch (window.location.hostname) {
-
-    // this is the local host name of your react app
-    case 'localhost' || '127.0.0.1':
-        // this is the local host name of your API
-        APIURL = 'http://localhost:4000';
-        break;
-        
-        //uyr7tf7t
-        // this is the deployed react application
-    case 'https://ajs-bowlinglog-client.herokuapp.com':
-        // this is the full url of your deployed API
-        APIURL = 'https://ajs-bowlinglog-server.herokuapp.com';
-    
+if (window.location.hostname == 'localhost' || window.location.hostname == '127.0.0.1') {
+    APIURL = 'http://localhost:4000';
+} else {
+    APIURL = 'https://ajs-bowlinglog-server.herokuapp.com'
 }
 
 export default APIURL;
